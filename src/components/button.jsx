7a0c9e57ddfdbd45119ce420482ebee26811685a
juuuunny <102502542+juuuunny@users.noreply.github.com>
2023-11-16 20:39:15 +0900

    [feat] 로그인 박스 완료

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -2,13 +2,13 @@ import styled from "styled-components";
 
 export const FillButton = styled.button`
   cursor: pointer;
-  color: #fff;
   text-align: center;
   font-family: "Pretendard";
   font-weight: 600;
   line-height: 24px;
-  border-radius: 8px;
   border: none;
+  border-radius: ${(props) => props.borderRadius || "8px"};
+  color: ${(props) => props.color || "#fff"};
   background-color: ${(props) => props.backgroundColor || null};
   font-size: ${(props) => props.fontSize || "22px"};
   width: ${(props) => props.width || "328px"};
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { LoginTotalComponent, Logo, LoginBox, TextDiv, TextInput, FlexBox } from "./style";
+import { FillButton } from "../../components/button";
 
 const Login = () => {
   return (
@@ -21,9 +22,30 @@ const Login = () => {
           >
             로그인
           </TextDiv>
-          <TextInput placeholder="아이디" marginTop="16px" />
+          <TextInput placeholder="아이디" marginTop="12px" />
           <TextInput placeholder="비밀번호" marginTop="8px" />
-        </FlexBox>{" "}
+          <FillButton
+            backgroundColor="#FFA7A7"
+            width="304px"
+            height="44px"
+            borderRadius="6px"
+            style={{ marginTop: "18px" }}
+          >
+            로그인
+          </FillButton>
+          <FlexBox style={{ marginTop: "20px" }}>
+            <TextDiv fontSize="14px" fontWeight="400">
+              비밀번호 찾기
+            </TextDiv>
+            <TextDiv style={{ margin: "0px 7px" }}>|</TextDiv>
+            <TextDiv fontSize="14px" fontWeight="400">
+              아이디 찾기
+            </TextDiv>
+            <TextDiv fontSize="14px" fontWeight="400" style={{ marginLeft: "78px" }}>
+              회원가입
+            </TextDiv>
+          </FlexBox>
+        </FlexBox>
       </LoginBox>
     </LoginTotalComponent>
   );
