40b3a042c29cb46097972051ef27bd4710602d9e
juuuunny <102502542+juuuunny@users.noreply.github.com>
2023-11-28 14:35:56 +0900

    [feat]취미 클릭까지 구현

--- src/pages/signup/SecondAddition.jsx
+++ src/pages/signup/SecondAddition.jsx
@@ -18,6 +18,85 @@ const SecondAddition = () => {
     relationType: "",
   });
 
+  const handleChangeHobby = (e) => {
+    if (e.target.value === "등산") {
+      setUserInfo({
+        ...userInfo,
+        hobby: "HIKE",
+      });
+    } else if (e.target.value === "수다") {
+      setUserInfo({
+        ...userInfo,
+        hobby: "TALK",
+      });
+    } else if (e.target.value === "맛집탐방") {
+      setUserInfo({
+        ...userInfo,
+        hobby: "EAT",
+      });
+    } else if (e.target.value === "여행") {
+      setUserInfo({
+        ...userInfo,
+        hobby: "TRAVEL",
+      });
+    } else if (e.target.value === "야구관람") {
+      setUserInfo({
+        ...userInfo,
+        hobby: "WATCH_BASEBALL",
+      });
+    } else if (e.target.value === "악기연주") {
+      setUserInfo({
+        ...userInfo,
+        hobby: "MUSICAL_INSTRUMENT",
+      });
+    } else if (e.target.value === "요리") {
+      setUserInfo({
+        ...userInfo,
+        hobby: "COOK",
+      });
+    } else if (e.target.value === "드라이브") {
+      setUserInfo({
+        ...userInfo,
+        hobby: "DRIVE",
+      });
+    } else if (e.target.value === "산책") {
+      setUserInfo({
+        ...userInfo,
+        hobby: "WALK",
+      });
+    } else if (e.target.value === "전시관람") {
+      setUserInfo({
+        ...userInfo,
+        hobby: "EXHIBITION",
+      });
+    } else if (e.target.value === "독서") {
+      setUserInfo({
+        ...userInfo,
+        hobby: "BOOK",
+      });
+    } else if (e.target.value === "운동") {
+      setUserInfo({
+        ...userInfo,
+        hobby: "WORK_OUT",
+      });
+    } else if (e.target.value === "미술") {
+      setUserInfo({
+        ...userInfo,
+        hobby: "FINE_ART",
+      });
+    } else if (e.target.value === "재테크") {
+      setUserInfo({
+        ...userInfo,
+        hobby: "INVESTMENT",
+      });
+    } else if (e.target.value === "사진촬영") {
+      setUserInfo({
+        ...userInfo,
+        hobby: "PHOTOGRAPY",
+      });
+    }
+  };
+
   return (
     <EmailSignupTotalComponent>
       <Link to="/" style={{ textDecoration: "none", color: "black" }}>
@@ -43,137 +122,317 @@ const SecondAddition = () => {
         </TextLabel>
         <GridBox>
           {userInfo.hobby === "HIKE" ? (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#FFA7A7" fontSize="16px">
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="등산"
+              backgroundColor="#FFA7A7"
+              fontSize="16px"
+            >
               등산
             </NoWidthHeightButton>
           ) : (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#949494" fontSize="16px">
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="등산"
+              backgroundColor="#949494"
+              fontSize="16px"
+            >
               등산
             </NoWidthHeightButton>
           )}
-          {userInfo.hobby === "HIKE" ? (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#FFA7A7" fontSize="16px">
+          {userInfo.hobby === "TALK" ? (
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="수다"
+              backgroundColor="#FFA7A7"
+              fontSize="16px"
+            >
               수다
             </NoWidthHeightButton>
           ) : (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#949494" fontSize="16px">
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="수다"
+              backgroundColor="#949494"
+              fontSize="16px"
+            >
               수다
             </NoWidthHeightButton>
           )}
-          {userInfo.hobby === "HIKE" ? (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#FFA7A7" fontSize="16px">
+          {userInfo.hobby === "EAT" ? (
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="맛집탐방"
+              backgroundColor="#FFA7A7"
+              fontSize="16px"
+            >
               맛집탐방
             </NoWidthHeightButton>
           ) : (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#949494" fontSize="16px">
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="맛집탐방"
+              backgroundColor="#949494"
+              fontSize="16px"
+            >
               맛집탐방
             </NoWidthHeightButton>
           )}
-          {userInfo.hobby === "HIKE" ? (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#FFA7A7" fontSize="16px">
+          {userInfo.hobby === "TRAVEL" ? (
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="여행"
+              backgroundColor="#FFA7A7"
+              fontSize="16px"
+            >
               여행
             </NoWidthHeightButton>
           ) : (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#949494" fontSize="16px">
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="여행"
+              backgroundColor="#949494"
+              fontSize="16px"
+            >
               여행
             </NoWidthHeightButton>
           )}
-          {userInfo.hobby === "HIKE" ? (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#FFA7A7" fontSize="16px">
+          {userInfo.hobby === "WATCH_BASEBALL" ? (
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="야구관람"
+              backgroundColor="#FFA7A7"
+              fontSize="16px"
+            >
               야구 관람
             </NoWidthHeightButton>
           ) : (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#949494" fontSize="16px">
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="야구관람"
+              backgroundColor="#949494"
+              fontSize="16px"
+            >
               야구 관람
             </NoWidthHeightButton>
           )}
-          {userInfo.hobby === "HIKE" ? (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#FFA7A7" fontSize="16px">
+          {userInfo.hobby === "MUSICAL_INSTRUMENT" ? (
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="악기연주"
+              backgroundColor="#FFA7A7"
+              fontSize="16px"
+            >
               악기 연주
             </NoWidthHeightButton>
           ) : (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#949494" fontSize="16px">
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="악기연주"
+              backgroundColor="#949494"
+              fontSize="16px"
+            >
               악기 연주
             </NoWidthHeightButton>
           )}
-          {userInfo.hobby === "HIKE" ? (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#FFA7A7" fontSize="16px">
+          {userInfo.hobby === "COOK" ? (
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="요리"
+              backgroundColor="#FFA7A7"
+              fontSize="16px"
+            >
               요리
             </NoWidthHeightButton>
           ) : (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#949494" fontSize="16px">
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="요리"
+              backgroundColor="#949494"
+              fontSize="16px"
+            >
               요리
             </NoWidthHeightButton>
           )}
-          {userInfo.hobby === "HIKE" ? (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#FFA7A7" fontSize="16px">
+          {userInfo.hobby === "DRIVE" ? (
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="드라이브"
+              backgroundColor="#FFA7A7"
+              fontSize="16px"
+            >
               드라이브
             </NoWidthHeightButton>
           ) : (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#949494" fontSize="16px">
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="드라이브"
+              backgroundColor="#949494"
+              fontSize="16px"
+            >
               드라이브
             </NoWidthHeightButton>
           )}
-          {userInfo.hobby === "HIKE" ? (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#FFA7A7" fontSize="16px">
+          {userInfo.hobby === "WALK" ? (
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="산책"
+              backgroundColor="#FFA7A7"
+              fontSize="16px"
+            >
               산책
             </NoWidthHeightButton>
           ) : (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#949494" fontSize="16px">
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="산책"
+              backgroundColor="#949494"
+              fontSize="16px"
+            >
               산책
             </NoWidthHeightButton>
           )}
-          {userInfo.hobby === "HIKE" ? (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#FFA7A7" fontSize="16px">
+          {userInfo.hobby === "EXHIBITION" ? (
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="전시관람"
+              backgroundColor="#FFA7A7"
+              fontSize="16px"
+            >
               전시 관람
             </NoWidthHeightButton>
           ) : (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#949494" fontSize="16px">
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="전시관람"
+              backgroundColor="#949494"
+              fontSize="16px"
+            >
               전시 관람
             </NoWidthHeightButton>
           )}
-          {userInfo.hobby === "HIKE" ? (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#FFA7A7" fontSize="16px">
+          {userInfo.hobby === "BOOK" ? (
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="독서"
+              backgroundColor="#FFA7A7"
+              fontSize="16px"
+            >
               독서
             </NoWidthHeightButton>
           ) : (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#949494" fontSize="16px">
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="독서"
+              backgroundColor="#949494"
+              fontSize="16px"
+            >
               독서
             </NoWidthHeightButton>
           )}
-          {userInfo.hobby === "HIKE" ? (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#FFA7A7" fontSize="16px">
+          {userInfo.hobby === "WORK_OUT" ? (
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="운동"
+              backgroundColor="#FFA7A7"
+              fontSize="16px"
+            >
               운동
             </NoWidthHeightButton>
           ) : (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#949494" fontSize="16px">
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="운동"
+              backgroundColor="#949494"
+              fontSize="16px"
+            >
               운동
             </NoWidthHeightButton>
           )}
-          {userInfo.hobby === "HIKE" ? (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#FFA7A7" fontSize="16px">
+          {userInfo.hobby === "FINE_ART" ? (
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="미술"
+              backgroundColor="#FFA7A7"
+              fontSize="16px"
+            >
               미술
             </NoWidthHeightButton>
           ) : (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#949494" fontSize="16px">
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="미술"
+              backgroundColor="#949494"
+              fontSize="16px"
+            >
               미술
             </NoWidthHeightButton>
           )}
-          {userInfo.hobby === "HIKE" ? (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#FFA7A7" fontSize="16px">
+          {userInfo.hobby === "INVESTMENT" ? (
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="재테크"
+              backgroundColor="#FFA7A7"
+              fontSize="16px"
+            >
               재테크
             </NoWidthHeightButton>
           ) : (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#949494" fontSize="16px">
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="재테크"
+              backgroundColor="#949494"
+              fontSize="16px"
+            >
               재테크
             </NoWidthHeightButton>
           )}
-          {userInfo.hobby === "HIKE" ? (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#FFA7A7" fontSize="16px">
+          {userInfo.hobby === "PHOTOGRAPY" ? (
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="사진촬영"
+              backgroundColor="#FFA7A7"
+              fontSize="16px"
+            >
               사진 촬영
             </NoWidthHeightButton>
           ) : (
-            <NoWidthHeightButton margin="2px 6px" backgroundColor="#949494" fontSize="16px">
+            <NoWidthHeightButton
+              onClick={handleChangeHobby}
+              margin="2px 6px"
+              value="사진촬영"
+              backgroundColor="#949494"
+              fontSize="16px"
+            >
               사진 촬영
             </NoWidthHeightButton>
           )}
