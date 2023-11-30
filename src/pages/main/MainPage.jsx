6e54078bf25cfa839156b487ce893f9fd1c45b0f
juuuunny <102502542+juuuunny@users.noreply.github.com>
2023-11-30 15:13:09 +0900

    [feat] 메인페이지 추천친구 리스트 띄우기 구현

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { FriendBox, MainTotalComponent, TextParagraph } from "./style";
+import { FriendBox, MainTotalComponent, TextAlignRightDiv, TextParagraph } from "./style";
 import { Header } from "../../layouts/header/Header";
 import { Footer } from "../../layouts/footer/Footer";
-import { FillButton } from "../../components/Button";
+import { FillButton, NoWidthHeightButton } from "../../components/Button";
 // import { TodayFriendsApi } from "../../apis/FriendApi";
+import OldPerson from "../../assets/main/OldPerson.svg";
 
 const MainPage = () => {
   const [todayFriendsList, setTodayFriendsList] = useState(null);
-
+  // const today = new Date();
   const getTodayFriends = async () => {
     // try {
     //   await TodayFriendsApi().then((res) => {
@@ -52,7 +53,7 @@ const MainPage = () => {
   }, []);
 
   return (
-    <MainTotalComponent>
+    <MainTotalComponent padding="0px 0px 130px 0px">
       <Header />
       <FillButton
         margin="86px 0px 0px 16px"
@@ -67,10 +68,36 @@ const MainPage = () => {
       {todayFriendsList !== null ? (
         todayFriendsList.map((item) => {
           return (
-            <FriendBox key={item.nickname}>
-              <TextParagraph>
+            <FriendBox key={item.nickname} backgroundImg={OldPerson} margin="16px 0px 0px 0px">
+              <TextParagraph margin="280px 0px 0px 16px" fontWeight="700">
                 {item.nickname}, {item.birth}
               </TextParagraph>
+              <TextAlignRightDiv>
+                <NoWidthHeightButton
+                  backgroundColor="rgb(255, 167, 167)"
+                  margin="10px 3px 0px"
+                  padding="4px 6px"
+                  fontSize="16px"
+                >
+                  {item.personality}
+                </NoWidthHeightButton>
+                <NoWidthHeightButton
+                  backgroundColor="#ffa7a7"
+                  margin="10px 3px 0px"
+                  padding="4px 6px"
+                  fontSize="16px"
+                >
+                  {item.hobby}
+                </NoWidthHeightButton>
+                <NoWidthHeightButton
+                  backgroundColor="#ffa7a7"
+                  margin="10px 3px 0px"
+                  padding="4px 6px"
+                  fontSize="16px"
+                >
+                  {item.relationType}
+                </NoWidthHeightButton>
+              </TextAlignRightDiv>
             </FriendBox>
           );
         })
